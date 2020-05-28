6a7a1f14b0e8b4fdfe25b9f4648df138ff119dea
nyano <nayan100@hotmail.co.uk>
2020-05-28 12:03:28 +0100

    Cleaned code to functions, colouring and deleting grid have been implemented

---
+++ code2.js
@@ -0,0 +1,66 @@
+size = 3
+
+// Make a 16*16 grid
+
+function makeRows(rows, cols){
+
+    const container = document.getElementById("container");
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+
+    for (let i =0; i<(rows*cols); i++){
+        let cell = document.createElement("div")
+        cell.textContent = i
+        cell.setAttribute('id',i)
+        container.appendChild(cell).className = "grid-item"
+}
+
+}
+
+makeRows(size,size)
+
+
+function change_grid_colour(){
+
+    let divs = document.querySelectorAll('div');
+    let grid = document.getElementsByClassName("grid-item");
+
+    let p = Array.from(grid)
+
+    // console.log(p)
+    // console.log(grid)
+    // console.log(divs)
+
+
+    p.forEach((div) => {
+
+        div.addEventListener('mouseenter', (e) => {
+            e.target.style.backgroundColor="purple"
+    })
+    })
+}
+
+function reset_grid(){
+
+    const btn = document.querySelector('#btn');
+    btn.addEventListener('click', () => {
+
+    let container = document.getElementById("container");
+    console.log(container)
+
+    while (container.firstElementChild){
+        container.removeChild(container.lastChild)
+    }
+    
+    input = window.prompt("What size do you want to the grid to be?")
+    makeRows(input, input)
+
+    change_grid_colour()
+
+
+})
+}
+
+
+change_grid_colour()
+reset_grid()
